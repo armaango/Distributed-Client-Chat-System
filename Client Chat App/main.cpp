40882959664d0133fc4fd32774f30c8456e5de4d
Armaan Goyal <AmmY@armaans-macbook-pro.local>
2016-03-21 22:17:00 -0400

    Initial Commit

---
+++ Client Chat App/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Client Chat App
+//
+//  Created by Armaan Goyal on 21/03/16.
+//  Copyright © 2016 Armaan Goyal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
